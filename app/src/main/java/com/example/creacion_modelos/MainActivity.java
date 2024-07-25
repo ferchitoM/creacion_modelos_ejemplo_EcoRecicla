66378ba4afd31d2635dec3f64dc23f205b98c269
Miguel Fernando Marlés Betancourt <80717402+ferchitoM@users.noreply.github.com>
2024-07-24 22:21:04 -0500

    update

--- app/src/main/java/com/example/creacion_modelos/MainActivity.java
+++ app/src/main/java/com/example/creacion_modelos/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.creacion_modelos.models.Material;
 import com.example.creacion_modelos.models.Recycling;
@@ -20,70 +22,31 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView lista;
+    MaterialRecyclingAdapter adapter;
+    ArrayList<Material> baseMaterials;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //CREAR MATERIALES
-        Material papel = new Material("Papel", 1000);
-        Material plastico = new Material("Plástico", 2000);
-        Material vidrio = new Material("Vidrio", 500);
-        Material metal = new Material("Metal", 1500);
+        baseMaterials = new ArrayList<Material>();
+        baseMaterials.add(new Material("Papel", 1000));
+        baseMaterials.add(new Material("Cartón", 500));
+        baseMaterials.add(new Material("Metal", 1500));
+        baseMaterials.add(new Material("Plástico", 2000));
+        baseMaterials.add(new Material("Vidrio", 800));
 
-        //REGISTRAR UN USUARIO NUEVO
-        User newUser = new User();
-        newUser.name = "Pablo";
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
 
-        //REGISTRAR EL RECICLAJE DE HOY
-        Recycling reciclajeMesJulio = new Recycling();
+        lista = findViewById(R.id.lista);
+        lista   .setLayoutManager(layoutManager);
+        lista   .setHasFixedSize(true);
 
-        //AGREGAMOS UN KILO Y MEDIO DE PAPEL AL RECICLAJE
-        papel.weight    = 1.5;
-        papel.calculateGain(papel.price);
-        reciclajeMesJulio.addMaterial(papel);
+        adapter = new MaterialRecyclingAdapter(baseMaterials);
+        lista.setAdapter(adapter);
 
-        //AGREGAMOS UN KILO DE PLÁSTICO AL RECICLAJE
-        plastico.weight = 1.0;
-        plastico.calculateGain(plastico.price);
-        reciclajeMesJulio.addMaterial(plastico);
-
-        //AGREGAR EL RECICLAJE REGISTRADO AL USUARIO
-        newUser.addRecycling(reciclajeMesJulio);
-
-
-
-
-
-
-
-        //REGISTRAR EL RECICLAJE DE EL PROXIMO
-        Recycling reciclajeMesAgosto = new Recycling();
-
-        //AGREGAMOS UN KILO Y MEDIO DE PAPEL AL RECICLAJE
-        papel.weight    = 1.5;
-        papel.calculateGain(2000);
-        reciclajeMesAgosto.addMaterial(papel);
-
-        //AGREGAMOS UN KILO DE PLÁSTICO AL RECICLAJE
-        vidrio.weight = 0.5;
-        vidrio.calculateGain(800);
-        reciclajeMesAgosto.addMaterial(vidrio);
-
-        //AGREGAMOS UN KILO DE METAL AL RECICLAJE
-        metal.weight = 1;
-        metal.calculateGain(1800);
-        reciclajeMesAgosto.addMaterial(metal);
-
-        //AGREGAR EL RECICLAJE REGISTRADO AL USUARIO
-        newUser.addRecycling(reciclajeMesAgosto);
-
-        //MOSTRAR DATOS REGISTRADOS POR PANTALLA
-        newUser.showRecyclings();
-
-
-
-        //Mostrar por pantalla las estadísticas
 
     }
 }
---
+++ app/src/main/java/com/example/creacion_modelos/MaterialRecyclingAdapter.java
@@ -0,0 +1,126 @@
+package com.example.creacion_modelos;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.creacion_modelos.models.Material;
+import java.util.ArrayList;
+import java.util.zip.Inflater;
+
+public class MaterialRecyclingAdapter extends RecyclerView.Adapter<MaterialRecyclingAdapter.MyViewHolder> {
+
+    public ArrayList<Material> materials;
+
+    public MaterialRecyclingAdapter(ArrayList<Material> materials){
+        this.materials = materials;
+    }
+
+    @NonNull
+    @Override
+    public MaterialRecyclingAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view   = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.registrar_material_reciclaje, parent, false);
+
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MaterialRecyclingAdapter.MyViewHolder holder, int position) {
+
+        Material material = materials.get(position);
+        holder.nombreMaterial.setText(material.name);
+        holder.price.setText(material.price + "");
+        holder.weight.setText(material.weight + "");
+
+        material.calculateGain(material.price);
+        holder.gain.setText(material.gain + "");
+
+
+        holder.price.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // No se necesita hacer nada aquí
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                String value = holder.price.getText().toString();
+
+                if(value.length() > 0) {
+
+                    double newPrice = Double.parseDouble(value);
+
+                    material.calculateGain(newPrice);
+                    holder.gain.setText(material.gain + "");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+
+        });
+
+        holder.weight.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // No se necesita hacer nada aquí
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                String value = holder.weight.getText().toString();
+
+                if(value.length() > 0) {
+
+                    double newWeight = Double.parseDouble(value);
+
+                    material.weight = newWeight;
+                    material.calculateGain(material.price);
+                    holder.gain.setText(material.gain + "");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.materials.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView nombreMaterial;
+        TextView price;
+        TextView weight;
+        TextView gain;
+
+        public MyViewHolder(@NonNull View item) {
+            super(item);
+
+            nombreMaterial  = item.findViewById(R.id.nombreMaterial);
+            price           = item.findViewById(R.id.price);
+            weight          = item.findViewById(R.id.weight);
+            gain            = item.findViewById(R.id.gain);
+        }
+    }
+}
